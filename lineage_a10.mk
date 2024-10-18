d6cf59816d0c3ea284ba675224ea428fc8aa273a
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    a10: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_a10.mk
+++ lineage_a10.mk
@@ -13,14 +13,13 @@ $(call inherit-product, device/samsung/a10/device.mk)
 # Inherit LineageOS common device config
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-BUILD_FINGERPRINT := "samsung/a10xx/a10:11/RP1A.200720.012/A105FNXXS9CWB2:user/release-keys"
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-   BuildDesc="a10xx-user 11 RP1A.200720.012 A105FNXXS9CWB2 release-keys"
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a10
 PRODUCT_NAME := lineage_a10
 PRODUCT_MODEL := SM-A105F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="a10xx-user 11 RP1A.200720.012 A105FNXXS9CWB2 release-keys" \
+    BuildFingerprint=samsung/a10xx/a10:11/RP1A.200720.012/A105FNXXS9CWB2:user/release-keys
