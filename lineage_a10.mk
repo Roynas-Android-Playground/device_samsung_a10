566f76285e875b08570d7cfb58af0269c5444733
nhglong <its.nhglong@gmail.com>
2025-09-17 10:59:18 +0000

    a10: Normalize BuildDesc

--- lineage_a10.mk
+++ lineage_a10.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 BUILD_FINGERPRINT := "samsung/a10kx/a10:11/RP1A.200720.012/A105NKOU5CUL4:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-   BuildDesc=$(call normalize-path-list, "a10kx-user 11 RP1A.200720.012 A105NKOU5CUL4 release-keys")
+   BuildDesc="a10kx-user 11 RP1A.200720.012 A105NKOU5CUL4 release-keys"
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a10
